663ee5c9ad519828d43d2b1650c4d09a2c8e8962
Logan Del Tillman <ltillma4@hydra5.eecs.utk.edu>
2020-03-11 15:24:14 -0400

    Fixed commenting and formatting issues

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,4 +1,10 @@
 // dijsktras.cpp
+// Project 4
+// Authors: Logan Tillman, Grant Anderson
+// Date: March 11, 2020
+
+/* This program will take a graph through standard input, along with a starting point and ending point.
+   It will use Dijkstras algorithm to create the shortest path between the two points. */
 
 #include <iostream>
 #include <vector>
@@ -7,10 +13,10 @@
 #include <ctype.h>
 using namespace std;
 
+// Used to store our Node Information
 struct Node
 {
 	Node(int ind, char t, int w);
-
 	Node *backedge;
 	bool visited;
 	char tile;
@@ -20,6 +26,7 @@ struct Node
 	vector <Node *> adjacent;
 };
 
+// Used to store information about our graph
 struct Matrix
 {
 	public:
@@ -36,89 +43,22 @@ struct Matrix
 };
 
 // Main Execution
-
 int main(int argc, char *argv[]) 
 {
 	unsigned int numtiles;
 
 	cin >> numtiles;
 
+	// Creating the graph
 	Matrix landscape(numtiles);
+
 	landscape.FindEdges();
 	landscape.CreatePath();
 
 	return 0;
 }
 
-void Matrix::CreatePath(){
-
-	Node *n;
-	n = graph[end];	
-
-	vector<int> flow;
-	int totalWeight = 0;
-
-	while(true)
-	{
-		if (n->index != (int) end)
-			totalWeight += n->weight;
-
-		flow.push_back(n->index);
-
-		if (n->index == (int) start)
-			break;
-		else
-			n = n->backedge;
-	}
-
-	cout << totalWeight << '\n';
-
-//	cout << flow[0]/cols << " " << flow[0]%cols << '\n';
-	for(int i = flow.size()-1; i >= 0; i--)
-		cout << flow[i]/cols << ' ' << flow[i]%cols << '\n';
-
-}
-
-void Matrix::FindEdges(){
-
-	multimap<int, Node *> Nodes;
-	multimap<int, Node *>::iterator nit;
-
-	graph[0]->distance = 0;
-	Node *n;
-	n = graph[0];
-	Nodes.insert(pair<int, Node *>(0, n));
-
-	while(!Nodes.empty()){
-		
-		nit = Nodes.begin();
-		nit->second->visited = true;
-
-		int dist = nit->second->distance;
-
-		for(unsigned int j = 0; j < nit->second->adjacent.size(); j++){
-
-			if(nit->second->adjacent[j]->visited == false){
-				if((dist + nit->second->adjacent[j]->weight) < nit->second->adjacent[j]->distance || nit->second->adjacent[j]->distance == -1){
-
-					nit->second->adjacent[j]->distance = dist + nit->second->adjacent[j]->weight;
-					nit->second->adjacent[j]->backedge = nit->second;
-
-					int a = nit->second->adjacent[j]->index;
-					Nodes.insert(pair<int, Node *>(nit->second->adjacent[j]->distance, graph[a]));
-
-				}
-			}
-
-		}
-
-		nit = Nodes.begin();
-		Nodes.erase(nit);
-	}
-
-	return;
-}
-
+// Node Constructor
 Node::Node(int ind, char t, int w)
 {
 	backedge = NULL;
@@ -130,6 +70,7 @@ Node::Node(int ind, char t, int w)
 	weight = w;
 }
 
+// Matrix Constructor
 Matrix::Matrix(unsigned int numtiles)
 {
 	char tilename, inputtile;
@@ -163,7 +104,7 @@ Matrix::Matrix(unsigned int numtiles)
 	for (int i = 0; i < (int) graph.size(); i++)
 	{
 		// Adding above node to adj
-		if ((i - cols) >= 0 && (i-cols) < (rows * cols))
+		if ((i-cols) >= 0 && (i-cols) < (rows*cols))
 			graph[i]->adjacent.push_back(graph[i-cols]);
 
 		// Adding left node to adj
@@ -175,46 +116,106 @@ Matrix::Matrix(unsigned int numtiles)
 			graph[i]->adjacent.push_back(graph[i+1]);
 
 		// Adding below node to adj
-		if ((i + cols) < (rows * cols))
+		if ((i+cols) < (rows*cols))
 			graph[i]->adjacent.push_back(graph[i+cols]);
 	}
 
 	cin >> startrow >> startcol >> endrow >> endcol;
+
+	// Converting the start/end rows and cols to fit our 1d vector format
 	end = (endrow*rows)+endcol;
 	start = (startcol*rows)+startcol;
-
-	/* -- Testing input -- */
-	/*	cout << numtiles << '\n';
-
-		for (tileiterator = tilecosts.begin(); tileiterator != tilecosts.end(); tileiterator++)
-		cout << tileiterator->first << " " << tileiterator->second << '\n';
-
-		cout << rows << " " << cols << '\n';
-
-		for (unsigned int i = 0; i < graph.size(); i++)
-		{
-		if ((i % cols == 0) && i != 0)
-		cout << '\n';
-		cout << graph[i]->tile << " ";
-		}
-		cout << '\n';
-		cout << startrow << " " << startcol << '\n';
-		cout << endrow << " " << endcol << '\n';
-
-		-- Checking the adjacent matrix -- 
-		for (unsigned int i = 0; i < graph.size(); i++)
-		{
-		cout << "graph[" << i << "] : ";
-		for (unsigned int j = 0; j < graph[i]->adjacent.size(); j++)
-		cout << graph[i]->adjacent[j]->tile << " ";
-
-		cout << '\n';
-		}
-		*/
 }
 
+// Destructor
 Matrix::~Matrix()
 {
+	// Deleting all of the nodes in the graph
 	for (unsigned int i = 0; i < graph.size(); i++)
 		delete graph[i];
+}
+
+// Finding the edges in the graph
+void Matrix::FindEdges()
+{
+    multimap<int, Node *> Nodes;
+    multimap<int, Node *>::iterator nit;
+	Node *n;
+
+	// Initializing the first node
+    graph[0]->distance = 0;
+    n = graph[0];
+
+	// Inserting it into the multimap
+    Nodes.insert(pair<int, Node *>(n->distance, n));
+
+    while(!Nodes.empty())
+	{
+		int dist;
+
+        nit = Nodes.begin();
+        nit->second->visited = true;
+        dist = nit->second->distance;
+
+		// Looping through all adjacent nodes
+        for(unsigned int j = 0; j < nit->second->adjacent.size(); j++)
+		{
+			// If the node hasn't been visited
+            if(nit->second->adjacent[j]->visited == false)
+			{
+				// If the node's weight < its distance (or if its distance is -1)
+                if((dist+nit->second->adjacent[j]->weight) < nit->second->adjacent[j]->distance || 
+				   (nit->second->adjacent[j]->distance == -1))
+				{
+					int a;
+					
+					// Updating its distance
+                    nit->second->adjacent[j]->distance = dist + nit->second->adjacent[j]->weight;
+
+					// Setting its backedge
+                    nit->second->adjacent[j]->backedge = nit->second;
+
+					// Inserting the node into the multimap
+                    a = nit->second->adjacent[j]->index;
+                    Nodes.insert(pair<int, Node *>(nit->second->adjacent[j]->distance, graph[a]));
+                }
+            }
+        }
+
+        nit = Nodes.begin();
+        Nodes.erase(nit);
+    }
+}
+
+// Constructing the shortest path between the start and end
+void Matrix::CreatePath()
+{
+    vector<int> flow;
+    int totalWeight;
+    Node *n;
+
+	// Starting at the ending node
+    n = graph[end];
+	totalWeight = 0;
+
+	// Calculating the total distance and putting all of the path nodes into a vector
+    while(true)
+    {
+        if (n->index != (int) end)
+            totalWeight += n->weight;
+
+        flow.push_back(n->index);
+
+        if (n->index == (int) start)
+            break;
+        else
+            n = n->backedge;
+    }
+
+	// Output formatting
+    cout << totalWeight << '\n';
+
+    for(int i = flow.size()-1; i >= 0; i--)
+        cout << flow[i]/cols << ' ' << flow[i]%cols << '\n';
+
 }

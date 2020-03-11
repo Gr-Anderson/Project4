9c4e44c402ebb805892da4d183a7f61d8a40e46c
Gr-Anderson <gander23@vols.utk.edu>
2020-03-10 20:48:06 -0400

    //fixed compiling issue

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
 	
 	cout << startrow <<  " " << startcol << '\n' << endrow << " " << endcol << '\n';*/
 
-	Nodes.insert(pair<int, int>(0, 0));
+//	Nodes.insert(pair<int, int>(0, 0));
 
 	for(int i = startrow; i < maprows; i++){
 		for(int j = startcol; j < mapcols; j++){
@@ -81,7 +81,7 @@ int main(int argc, char *argv[])
 					Nodes.insert(pair<int, int>(tileit->second, (i*mapcols)+j));
 						}
 					}
-/*			if(i-1 >= 0){
+		/*	if(i-1 >= 0){
 				tileit = tilecosts.find(matrix[i-1][j]);
 				if(tileit->second > 0)
 					Nodes.insert(pair<int, int>(tileit->second, (i*mapcols)+j));
@@ -94,30 +94,32 @@ int main(int argc, char *argv[])
 						}*/
 		}
 
-	i = (mapcols*endrow)+endcol;
+	int i = (mapcols*endrow)+endcol;
 	edges.push_back(i);
 	int total = 0;
 			
 	while(i != 0){
 		
 		for(Nodesit = Nodes.begin(); Nodesit != Nodes.end(); Nodesit++){
-			if(Nodesit->second == i-10){
-				edges.push_back(Nodesit->second);
-				i = Nodesit->second;
-				total += Nodesit->first;
-				Nodes.erase(Nodesit);
-				Nodesit = Nodes.begin();
-				if(i == 0)
-					break;
-			}
 			if(Nodesit->second == i-1){
+				if(i == 0)
+					break;
 				edges.push_back(Nodesit->second);
 				i = Nodesit->second;
 				total += Nodesit->first;
+				cout << total << '\n';
 				Nodes.erase(Nodesit);
 				Nodesit = Nodes.begin();
+			}
+			if(Nodesit->second == i-10){
 				if(i == 0)
 					break;
+				edges.push_back(Nodesit->second);
+				i = Nodesit->second;
+				total += Nodesit->first;
+				cout << total << '\n';
+				Nodes.erase(Nodesit);
+				Nodesit = Nodes.begin();
 			}
 		/*	if(Nodesit->second == i+1){
 				edges.push_back(Nodesit->second);
